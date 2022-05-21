e6ef40239747a63e26ea1075bf1d8c28d4dc7f40
Dani2033 <daniel.rubio.fernandez@hotmail.com>
2022-05-21 13:54:38 +0200

    add to cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Cart;
+use Illuminate\Contracts\Session\Session as SessionSession;
+use Session;
 
 class ProductController extends Controller
 {
@@ -14,9 +17,33 @@ class ProductController extends Controller
 
         return view('product', ['products'=>$data]);
     }
+
     function detail($id)
     {
         $data = Product::find($id);
         return view('detail', ['product'=>$data]);
+    }
+
+    function addToCart(Request $req)
+    {
+        if ($req->session()->has('user'))
+        {
+            $cart = new Cart;
+            $cart->user_id = $req->session()->get('user')['id'];
+            $cart->product_id = $req->product_id;
+            $cart->save();
+            return redirect('/');
+        }
+        else
+        {
+            return redirect('/login');
+        }
+
+    }
+
+    static function cartItem()
+    {
+        $userId = Session::get('user')['id'];
+        return Cart::where('user_id', $userId)->count();
     }
 }
